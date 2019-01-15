b1c151967f259295038cb3055bc80fafdb4a42db
infriend <stive0118@sina.com>
2019-01-15 23:21:33 +0800

    Add files via upload

--- data_structure/数据结构课程设计.cpp
+++ data_structure/数据结构课程设计.cpp
@@ -10,7 +10,7 @@
 2．对二叉排序树T作递增、递减输出 finish
 3. 计算二叉排序树T的平均查找长度，输出结果 finish
 4．在二叉排序树T上删除指定结点 finish
-5．输出二叉排序树T中指定结点的平衡因子
+5．输出二叉排序树T中指定结点的平衡因子 finish
 6．写出课程设计报告
 【测试数据】
 分别对选定两组测试数据进行测试，验证程序的正确性。
@@ -18,7 +18,6 @@
 
 #include <cstdio>
 #include <cstdlib>
-#include <queue>
 #include <vector>
 using namespace std;
 
@@ -28,7 +27,55 @@ typedef struct bitree
     int data;
 } btnode, *bst;
 
-int searchBST(bst tree, int e, bst &f, bst &p)//引用变量p
+typedef struct qnode
+{
+    bitree data;
+    struct qnode *next;
+} qnode;
+typedef struct myqueue
+{
+    qnode *front,*rear;
+} myqueue;
+
+void initque(myqueue *q)
+{
+    q->front = q->rear = (qnode*) malloc(sizeof(qnode));
+    q->front->next = NULL;//head node
+}
+
+void enqueue(myqueue *q, bst e)
+{
+    qnode *p;
+    p = (qnode*) malloc(sizeof(qnode));
+    p->data = *e;
+    p->next = NULL;
+    q->rear->next = p;
+    q->rear = p;
+}
+
+int emptyque(myqueue *q)
+{
+    if (q->rear == q->front)
+    {
+        return 1;
+    }
+    else return 0;
+}
+
+bitree dequeue(myqueue *q)
+{
+    qnode *p;
+    bitree e;
+    p = q->front->next;
+    e = p->data;
+    q->front->next = p->next;
+    if (q->rear == p) q->rear = q->front;
+    free(p);
+
+    return e;
+}
+
+int searchBST(bst tree, int e, bst &f, bst &p)
 {
 
     if (!tree)
@@ -76,48 +123,36 @@ void insertBST(bst &tree, int e)
 
 void inctra(bst tree)
 {
-    static int n = 0;
     if (tree==NULL) ;
     else
     {
-        if (n==6)
-        {
-            printf("\n");
-            n = 0;
-        }
         inctra(tree->lc);
         printf("%4d", tree->data);
-        ++n;
         inctra(tree->rc);
     }
 }
 
 void dectra(bst tree)
 {
-    static int n = 0;
     if (tree==NULL) ;
     else
     {
-        if (n==6)
-        {
-            printf("\n");
-            n = 0;
-        }
         dectra(tree->rc);
         printf("%4d", tree->data);
-        ++n;
         dectra(tree->lc);
     }
 }
 
-int lvcount(bst t)
+double slcount(bst t)
 {
-    queue<bst> q1;
+    btnode p;
     vector<int> v1;
+    myqueue q;
+    initque(&q);
     int n = 1, sym = 0, res = 0, num = 0;
 
-    q1.push(t);
-    while (!q1.empty())
+    enqueue(&q, t);
+    while (!emptyque(&q))
     {
         if (sym == 0)
         {
@@ -125,17 +160,17 @@ int lvcount(bst t)
             sym = v1.back();
             n = 0;
         }
-        if (q1.front()->lc != NULL)
+        p = dequeue(&q);
+        if (p.lc != NULL)
         {
-            q1.push(q1.front()->lc);
+            enqueue(&q, p.lc);
             n++;
         }
-        if (q1.front()->rc != NULL)
+        if (p.rc != NULL)
         {
-            q1.push(q1.front()->rc);
+            enqueue(&q, p.rc);
             n++;
         }
-        q1.pop();
         sym--;
     }
     v1.push_back(n);
@@ -146,7 +181,7 @@ int lvcount(bst t)
         num += v1[i];
     }
 
-    return res/num;
+    return (double) res/(double) num;
 }
 
 bst delnode(bst tree, int e)
@@ -187,14 +222,15 @@ bst delnode(bst tree, int e)
                         q = s;
                         s = s->rc;
                     }
-                    s->data = p->data;
+                    p->data = s->data;
+                    printf("p=%d\n", p->data);
                     if (q != p) q->rc = s->lc;
                     else q->lc = s->lc;
                     free(s);
                 }
             }
         }
-
+        tree = p;
     }
     else
     {
@@ -223,13 +259,14 @@ bst delnode(bst tree, int e)
                 }//node with only one child
                 else
                 {
-                    p = p->lc;
+                    q = p;
+                    s = p->lc;
                     while (s->rc)
                     {
                         q = s;
                         s = s->rc;
                     }
-                    s->data = p->data;
+                    p->data = s->data;
                     if (q != p) q->rc = s->lc;
                     else q->lc = s->lc;
                     free(s);
@@ -239,9 +276,7 @@ bst delnode(bst tree, int e)
     }
     return tree;
 }
-/*
-若为叶子节点，直接删，注意改parent的child为NULL
-*/
+
 int depthcount(bst t)
 {
     int max = 0, ld = 0, rd = 0;
@@ -270,17 +305,39 @@ int bfcount(bst tree, int e)
 
 int main()
 {
-    int a[10] = {5,7,9,8,6,4,1,3,2,10};
-    bst tree = NULL,f = NULL,p = NULL;
-    for (int i=0; i<10; ++i)
+    vector<int> v;
+    bst tree = NULL,f = tree,p = NULL;
+    int a;
+    printf("Enter the number list(0 means the end)\n");
+    scanf("%d", &a);
+    while (a>0)
     {
-        insertBST(tree, a[i]);
+        v.push_back(a);
+        scanf("%d", &a);
     }
-    searchBST(tree, 3, f, p);
+    for (int i=0; i<(int) v.size(); ++i)
+    {
+        insertBST(tree, v[i]);
+    }
+    printf("Enter the number you'd like to search:");
+    scanf("%d", &a);
+    searchBST(tree, a, f, p);
+    if (p == tree) f = p;
+    printf("p=%d,f=%d", p->data, f->data);
+    printf("\nINC list:\n");
     inctra(tree);
     printf("\n");
+    printf("DEC list:\n");
     dectra(tree);
-    printf("\nbf of %d is %d", 1, bfcount(tree, 1));
+    printf("\nAverage search length: %f", slcount(tree));
+    printf("\nEnter the del number:");
+    scanf("%d", &a);
+    tree = delnode(tree, a);
+    inctra(tree);
+    printf("\nOutput the balance factor:\n");
+    printf("Enter the number:");
+    scanf("%d", &a);
+    printf("bf of %d is %d\n", a, bfcount(tree, a));
 
     return 0;
 }
