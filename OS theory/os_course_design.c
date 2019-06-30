8cfc83060c0492a2b67aa1145ac57616d56d2db9
infriend <stive0118@sina.com>
2019-06-30 21:35:48 +0800

    Add files via upload

--- OS theory/os_course_design.c
+++ OS theory/os_course_design.c
@@ -46,11 +46,11 @@ void redir(int dir, char *filename)
 	}
 }
 
-void pipe_func(char *order1, char *order2, char *const *e)
+void pipe_func(char *order1, char *order2, char *e[])
 {
-	int fd[2];
-	pid_t pid;
-	pid = fork();
+	int fd[2],pid;
+	pipe(fd);
+	pid=fork();
 	if(pid > 0)
 	{
 		close(fd[0]);
@@ -63,8 +63,31 @@ void pipe_func(char *order1, char *order2, char *const *e)
 		close(fd[1]);
 		dup2(fd[0], 0);
 		execve(order2, NULL, e);
+		exit(0);
 	}
 }
+/*void pipefun(char *cmd1,char *cmd2,char *env[])
+{
+	int fd[2],pid;
+	pipe(fd);
+	pid=fork();
+	if(pid>0)
+	{
+		close(fd[0]);
+		dup2(fd[1],1);
+		execve(cmd1,NULL,env);
+		waitpid(pid,NULL,0);
+	}
+	else if(pid==0)
+	{
+		close(fd[1]);
+		dup2(fd[0],0);
+		execve(cmd2,NULL,env);
+		exit(0);
+	}
+	else
+		printf("create process error!\n");
+}*/
 
 int main()
 {
@@ -127,6 +150,7 @@ int main()
 						gpath[j++] = s[i++];
 					}
 					gpath[j] = '\0';
+					printf("\npath=%s\n", gpath);
 				}
 				else
 				{
@@ -134,23 +158,32 @@ int main()
 					strcat(path, gpath);
 					char *envp[] = {path, 0};
 					pid = fork();
+					
+					i = strlen(order1);
+					i++;
+
 					if (pid < 0) printf("Fork error!");
 					if (pid == 0)
 					{	
-
-						if (access(order1, X_OK) == 0)
-						{
-							i++;
-							if(s[i] == '<') redir(1, &s[i+1]);
-							if(s[i] == '>') redir(2, &s[i+1]);
-							if(s[i] == '|') 
+						
+						printf("%s",&s[i]);
+						if(s[i] == '<') redir(1, &s[i+1]);		
+						if(s[i] == '>') redir(2, &s[i+1]);
+						if(s[i] == '|') 
 							{
 								pipe = 1;
 								i++;
 								pipe_func(order1, &s[i+1],envp);
+								printf("2");
+								exit(0);
 							}
-							if (pipe == 0) execve(order1, NULL, envp);
-						}
+ 						if (pipe == 0) execve(order1, NULL, envp);
+						/*if (access(order1, X_OK) == 0)
+						{
+							
+							
+						}*/
+						
 					}
 					sleep(1);
 				}
