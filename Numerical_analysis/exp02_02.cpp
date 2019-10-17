0ee29eac6b1b32c682aa1139d2470bf77eb40bd7
infriend <stive0118@sina.com>
2019-10-17 20:12:53 +0800

    Update exp02_02.cpp

--- Numerical_analysis/exp02_02.cpp
+++ Numerical_analysis/exp02_02.cpp
@@ -2,31 +2,46 @@
 #include <cstdio>
 using namespace std;
 
-double n, x[5], y[5], list[11];
+double x[5], y[5], list[16];
+int p = 0;
 
-void difference_quotient(double a, double b, int t)
+double difference_quotient(int a, int b, int t)
 {
+    int temp = t;
     if ((b - a) == 1)
     {
-        
+        list[t] = (y[b] - y[a])/(x[b] - x[a]);
+        return list[t];
     }
     else
     {
-        difference_quotient(a, b - 1, t++);
-        difference_quotient(a + 1, b, t++);
+        double right = difference_quotient(a, b - 1, p++);
+        double left = difference_quotient(a + 1, b, p++);
+        list[temp] = (left - right)/(x[b] - x[a]);
+        return list[temp];
     }
 }
 
-
 int main()
 {
-    double up, down, result = 0, temp, t;
+    double up, down, result = 0, temp = 1, n;
+    int j = 3;
     cin >> n;
     for (int i = 0; i < 5; i++)
     {
         cin >> x[i] >> y[i];
     }
-
+    difference_quotient(0, 4, p++);
+    for (int i = 0; i < 5; i++)
+    {
+        if (i == 0) result += y[0];
+        else
+        {
+            temp *= n - x[i-1];
+            result += temp * list[j--];
+        }
+    }
+    cout << result;
 
     return 0;
 }
